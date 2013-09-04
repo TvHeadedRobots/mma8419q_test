2e1246334b9fced176bccdabe8cbb1c96b16072b
Skip Tabor <pneusolematic@mac.com>
2013-09-04 11:50:13 -0400

    i2c comms is working
    pulled EN pin high and added necessary delay

--- sketch_sep03a/sketch_sep03a.ino
+++ sketch_sep03a/sketch_sep03a.ino
@@ -4,29 +4,35 @@
 
 int sda = 18;
 int scl = 19;
+int en = 20;
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
+  pinMode(en, OUTPUT);
 }
 
 
 void loop() {
-  digitalWrite(scl, HIGH);
+  /*digitalWrite(scl, HIGH);
   digitalWrite(sda, HIGH);
   digitalWrite(sda, LOW);
-  digitalWrite(scl, LOW);
+  digitalWrite(scl, LOW);*/
+  digitalWrite(en, HIGH);
+  delay(10);
   
   Serial.println("Start MMA8491q test");
-  Wire.requestFrom(171, 8);
+  Wire.requestFrom(0x55, 3);
   
   while(Wire.available())    // slave may send less than requested
   { 
     Serial.println("start read");
-    char c = Wire.read();    // receive a byte as character
-    Serial.print(c);         // print the character
+    int c = Wire.read();    // receive a byte as character
+    Serial.println(c);         // print the character
   }
 
   delay(500);
+  digitalWrite(en, LOW);
+
 }
 
